5a6e8f5753aa49eb3c72989241fba90fa47c10e7
Denis Seleznev <hcodes@users.noreply.github.com>
2021-10-31 16:45:41 +0300

    Fixes for typings

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,20 @@
 import typescript from '@rollup/plugin-typescript';
 
-export default {
-  plugins: [
-    typescript()
-  ]
-}
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      format: 'cjs',
+      file: './dist/index.common.js'
+    },
+    plugins: [typescript({ tsconfig: './tsconfig.json' })]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      format: 'es',
+      file: './dist/index.esm.js'
+    },
+    plugins: [typescript({ tsconfig: './tsconfig.json' })]
+  }
+];
